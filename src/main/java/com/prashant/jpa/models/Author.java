39a00a335433f51711c55a61e5a42d6bdaecfd7b
Prashant Ranjan <prashantranjan@Prashants-MacBook-Air.local>
2025-12-23 07:34:27 +0530

    complete author table

--- src/main/java/com/prashant/jpa/models/Author.java
+++ src/main/java/com/prashant/jpa/models/Author.java
@@ -6,28 +6,39 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "AUTHOR_TBL")
 public class Author {
 
     @Id
-    @GeneratedValue(
-            strategy = GenerationType.TABLE,
-            generator = "author_id_gen"
-    )
-    @TableGenerator(
-            name = "author_id_gen",
-            table = "id_generator",
-            pkColumnName = "id_name",
-            valueColumnName = "id_value",
-            allocationSize = 1
-    )
+    @GeneratedValue
     private Integer id;
+    @Column(
+            name = "f_name",
+            length = 35
+    )
     private String firstName;
     private String lastName;
+
+    @Column(
+            unique = true,
+            nullable = false
+    )
     private String email;
     private Integer age;
+    @Column(
+            updatable = false,
+            nullable = false
+    )
+    private LocalDateTime createdAt;
+    @Column(
+            insertable = false
+    )
+    private LocalDateTime lastModified;
 
 }
