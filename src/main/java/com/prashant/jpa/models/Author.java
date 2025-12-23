7cda6bf4d0357cdfeb25007d28737aaf3cab7a59
Prashant Ranjan <prashantranjan@Prashants-MacBook-Air.local>
2025-12-23 07:03:48 +0530

    added deffrent sequence generatore

--- src/main/java/com/prashant/jpa/models/Author.java
+++ src/main/java/com/prashant/jpa/models/Author.java
@@ -14,12 +14,14 @@ public class Author {
 
     @Id
     @GeneratedValue(
-            strategy = GenerationType.SEQUENCE,
-            generator = "author_sequence"
+            strategy = GenerationType.TABLE,
+            generator = "author_id_gen"
     )
-    @SequenceGenerator(
-            name = "author_sequence",
-            sequenceName = "author_sequence",
+    @TableGenerator(
+            name = "author_id_gen",
+            table = "id_generator",
+            pkColumnName = "id_name",
+            valueColumnName = "id_value",
             allocationSize = 1
     )
     private Integer id;
